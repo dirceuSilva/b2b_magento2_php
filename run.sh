5176b59b7f41716118ca68e48a6971a0719af804
Leandro Nunes <dr1design@gmail.com>
2017-10-18 03:54:04 -0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,12 +8,10 @@ mageroot="/src"
 echo "Initializing Magento2 setup using MAGE_ROOT=$mageroot..."
 if [ "$M2SETUP_FORCE_EXECUTION" == "true" ] || [ ! -f "$mageroot/app/etc/config.php" ] || [ ! -f "$mageroot/app/etc/env.php" ]; then
   $DIR/magento-create-project.sh $mageroot
-
-  $DIR/magento-set-permissions.sh $mageroot
-
-  $DIR/magento-install.sh $mageroot
 fi
 
+$DIR/magento-set-permissions.sh $mageroot
+$DIR/magento-install.sh $mageroot
 $DIR/magento-deploy.sh $mageroot
 echo "The setup script has completed execution."
 
