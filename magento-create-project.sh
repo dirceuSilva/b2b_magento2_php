88271a53aebf9f9cc6af6037ff201123878062e8
Leandro Nunes <dr1design@gmail.com>
2017-10-18 04:09:25 -0200

    Update magento-create-project.sh

--- magento-create-project.sh
+++ magento-create-project.sh
@@ -7,12 +7,13 @@ fi
 
 targetdir=$1
 if [ ! -f "$targetdir/composer.json" ]; then 
-  echo "No composer.json found on $targetdir. Creating a new one with 'composer create-project'"
-  composer create-project \
-    --repository-url=https://repo.magento.com/ \
-    magento/project-enterprise-edition=2.1.7 \
-    $targetdir
-  exit 0
+    echo "No composer.json found on $targetdir. Creating a new one with 'composer create-project'"
+    composer create-project \
+        --repository-url=https://repo.magento.com/ \
+        magento/project-enterprise-edition=2.1.7 \
+        $targetdir
+    exit 0
 fi
 
-echo "There is a project already created on $targetdir. Nothing to do."
+echo "Found a composer.json on $targetdir. Installing packages dependencies..."
+composer install
